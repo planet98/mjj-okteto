ee050ed1555eec26872752405de29f187a7fa52c
planet98 <planet@hongxin.eu.org>
2023-11-03 10:05:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ export NEZHA_SERVER=${NEZHA_SERVER:-''}
 export NEZHA_PORT=${NEZHA_PORT:-''}
 export NEZHA_KEY=${NEZHA_KEY:-''}
 export TLS=${TLS:-'1'}
-export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
+export ARGO_DOMAIN=${ARGO_DOMAIN:-'okteto.zgws.gay'}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
-export ARGO_AUTH=${ARGO_AUTH:-''}
+export ARGO_AUTH=${ARGO_AUTH:-'{"AccountTag":"2f4dc092cfcc36e039b4c273a2846c50","TunnelSecret":"xwE8INWO7esPtKi1ybKm1czhf6d7pD8mHGDscOvMWjc=","TunnelID":"f8f6d5ce-0352-4c69-8d66-40723068a60b"}'}
 export WSPATH=${WSPATH:-'argo'}
 export UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 export CFIP=${CFIP:-'icook.hk'}
